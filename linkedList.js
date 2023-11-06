2c897387b2977f4b98b554fc040c396d89cdcd97
Potassium <brainiacgamer444@gmail.com>
2023-11-06 17:49:31 +0545

    Create methods to append, prepend, show size, return nodes as string and insert at given index

--- linkedList.js
+++ linkedList.js
@@ -1,22 +1,46 @@
 class LinkedList {
-  constructor(head = null) {
-    this.head = head;
+  constructor() {
+    this.head = null;
   }
 
   append(value) {
     //adds a new node containing value at the end of the node
+    this.head = new Node(value, this.head);
   }
 
   prepend(value) {
     //adds a new node containing value at the start of the node
+    let node = new Node(value);
+    let currentPointer;
+
+    if (!this.head) {
+      this.head = node;
+    } else {
+      currentPointer = this.head;
+
+      while (currentPointer.nextNode) {
+        currentPointer = currentPointer.nextNode;
+      }
+
+      currentPointer.nextNode = node;
+    }
   }
 
   size() {
     //returns the total number of nodes in the list
+    let currentPointer = this.head;
+    let count = 0;
+
+    while (currentPointer) {
+      count++;
+      currentPointer = currentPointer.nextNode;
+    }
+    return count;
   }
 
   head() {
     //returns the first node of the list
+    this;
   }
 
   tail() {
@@ -41,10 +65,44 @@ class LinkedList {
 
   toString() {
     //reprenst your LinkedList objects as strings to print them out and preview them in console
+    // format ( value ) -> ( value ) -> ( value ) -> null
+    let node = this.head;
+    let string = "";
+
+    while (node) {
+      string += `(${node.value}) -> `;
+      node = node.nextNode;
+    }
+    console.log(string, null);
   }
 
   insertAt(value, index) {
     //inserts a node with the provided value at the index of the list
+    if (index < 0) {
+      console.log("Error! Index cannot be less than 0");
+      return;
+    } else if (index > this.size()) {
+      console.log("Index is greater than the node size! The value will be inserted at the end of the node");
+      this.prepend(value);
+      return;
+    } else if (index === 0) {
+      this.append(value);
+      return;
+    }
+
+    const node = new Node(value);
+    let currentPointer,
+      previousPointer,
+      count = 0;
+    currentPointer = this.head;
+    while (count < index) {
+      previousPointer = currentPointer;
+      count++;
+      currentPointer = currentPointer.nextNode;
+    }
+
+    node.nextNode = currentPointer;
+    previousPointer.nextNode = node;
   }
 
   removeAt(index) {
@@ -58,3 +116,10 @@ class Node {
     this.nextNode = nextNode;
   }
 }
+
+const list1 = new LinkedList();
+list1.append(100);
+list1.prepend(200);
+list1.insertAt(300, 1);
+list1.toString();
+console.log("Size of the node is:", list1.size());
